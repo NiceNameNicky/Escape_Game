83ae8cf2c747007fef5a01d08a8720a91aabea29
Congzhou Du <fantasticnicky@gmail.com>
2021-03-18 23:18:25 -0400

    Shooting and moving

---
+++ Assets/Scripts/BossMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    void Update()
+    {
+        if (Input.GetKey("a"))
+        {
+            rb.rotation += 100.0f * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.rotation -= 100.0f * Time.deltaTime;
+        }
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(transform.up * moveSpeed);
+        }
+        if(Input.GetKey("s"))
+        {
+            rb.AddForce(transform.up * moveSpeed * -1);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //rb.rotation = movement.x;
+    }
+}
---
+++ Assets/Scripts/BossShooting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossShooting : MonoBehaviour
+{
+
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    private float bulletForce = 5f;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+}
